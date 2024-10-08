4cca7dfb4688475078f3805cfca14265b8db25f3
Natalia Giselle Curia <nataliagiselle.c@gmail.com>
2024-10-08 13:49:08 -0300

    -

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
+
+
 // -- desplazamiento de la web con la barra lateral.
 
 document.querySelectorAll('.page a').forEach(link => {
     link.addEventListener('click', function(event) {
-    
+        
         // Obtener el elemento destino
         const target = document.querySelector(this.getAttribute('href'));
         if (target) {
@@ -55,75 +57,129 @@ sections.forEach(section => {
     observer.observe(section);
 });
 
-//--carga de proyectos
+//carga de proyectos.
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     Promise.all([
         fetch("icons.json").then(response => response.json()),
         fetch("projects.json").then(response => response.json())
     ])
     .then(([icons, projectsData]) => {
+        //-- Cargar skills
+        const skills = document.querySelector(".skills");
+        const skillsul = document.createElement("ul");
+
+        icons.icons.forEach((icon) => {
+            if (icon.name === 'GitHub') {
+                return '';
+            }
+            const skill = document.createElement("li");
+            skill.innerHTML = `${icon.url}`;
+            skillsul.appendChild(skill);
+        });
+
+        skills.appendChild(skillsul);
+
+        //-- Carga de proyectos
+        const work = document.querySelector(".work");
         const slider = document.querySelector(".slider");
-        slider.style.display = 'flex';  // Asegurarse de que el slider funcione con flexbox
-        
-        // Iterar sobre los proyectos y generar las diapositivas
-        projectsData.projects.forEach((project, index) => {
-            const slide = document.createElement("div");
-            slide.classList.add("slide");
-            slide.id = `slide-${index + 1}`;
-    
+
+        // Filtrar proyectos
+        const personalProjects = projectsData.projects.filter(project => project.type === "project");
+        const workProjects = projectsData.projects.filter(project => project.type === "work");
+
+        workProjects.forEach((project, index) => {
+            const proj = document.createElement("div");
+            proj.classList.add("slide");
+            proj.id = `work-slide-${index + 1}`;
+
             // Generar la lista de íconos de tecnologías usando icons.json
             const techIcons = project.technologies.map(tech => {
+                
                 const icon = icons.icons.find(icon => icon.name === tech);
-                return icon ? `<img src="${icon.url}" alt="${tech}" title="${tech}">` : tech;
-            }).join(' '); // Asegurarse de que los íconos se muestren correctamente
-    
+                return icon ? icon.url : tech;
+            }).join(' ');
+
+            proj.innerHTML = `
+                <div class="project-description">
+                    <h5>${project.title}</h5>
+                    <p>${project.description}</p>
+                    <span class="tecnologies">${techIcons}</span>
+                    <span class="links">
+                        <a href="${project.liveDemo}" target="_blank">Ver Demo</a>
+                        <a href="${project.sourceCode}" target="_blank">GitHub</a>
+                        ${project.sourceCode2 ? `<a href="${project.sourceCode2}" target="_blank">Backend</a>` : ''}
+                    </span>
+                </div>
+                <div class="mockups">
+                    <div class="desktop">
+                        <img src="${project.imgDesktop}" alt="Vista de escritorio de ${project.title}">
+                    </div>
+                </div>
+            `;
+
+            work.appendChild(proj);
+        });
+
+        // Iterar sobre los proyectos personales
+        personalProjects.forEach((project, index) => {
+            const slide = document.createElement("div");
+            slide.classList.add("slide");
+            slide.id = `project-slide-${index + 1}`;
+
+            // Generar la lista de íconos de tecnologías 
+            const techIcons = project.technologies.map(tech => {
+                const icon = icons.icons.find(icon => icon.name === tech);
+                return icon ? icon.url : tech;
+            }).join(' ');
+
             slide.innerHTML = `
                 <div class="project-description">
                     <h5>${project.title}</h5>
                     <p>${project.description}</p>
                     <span class="tecnologies">${techIcons}</span>
                     <span class="links">
-                    <a href="${project.liveDemo}" target="_blank">Ver Demo</a>
-                    <a href="${project.sourceCode}" target="_blank">Código Fuente</a>
-                    ${project.sourceCode2 ? `<a href="${project.sourceCode2}" target="_blank">Backend</a>` : ''}
+                        <a href="${project.liveDemo}" target="_blank">Ver Demo</a>
+                        <a href="${project.sourceCode}" target="_blank">GitHub</a>
+                        ${project.sourceCode2 ? `<a href="${project.sourceCode2}" target="_blank">Backend</a>` : ''}
                     </span>
                 </div>
                 <div class="mockups">
                     <div class="desktop">
                         <img src="${project.imgDesktop}" alt="Vista de escritorio de ${project.title}">
-                        <div class="mobile ${project.imgMobile.includes('landscape') ? 'landscape' : ''}">
-                            <img src="${project.imgMobile}" alt="Vista móvil de ${project.title}">
-                        </div>
                     </div>
                 </div>
             `;
-    
+
             slider.appendChild(slide);
         });
 
-            //--Slider
-            let currentSlide = 0; 
-            const totalSlides = document.querySelectorAll('.slide').length; 
+        //-- Slider
+        let currentSlide = 0;
+        const totalSlides = personalProjects.length; // Contar solo los proyectos personales
 
-            function moveSlide(step) {
-                currentSlide += step;
+        function moveSlide(step) {
+            currentSlide += step;
 
-                if (currentSlide < 0) {
-                    currentSlide = totalSlides - 1; 
-                } else if (currentSlide >= totalSlides) {
-                    currentSlide = 0; 
-                }
-
-                const slideWidth = document.querySelector('.slide').clientWidth;
-
-                // Cambiar la posición del slider
-                slider.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
+            if (currentSlide < 0) {
+                currentSlide = totalSlides - 1;
+            } else if (currentSlide >= totalSlides) {
+                currentSlide = 0;
             }
 
-            document.querySelector('.prev').addEventListener('click', () => moveSlide(-1));
-            document.querySelector('.next').addEventListener('click', () => moveSlide(1));
-        })
-        .catch(error => console.error('Error loading projects:', error));
+            const slideWidth = document.querySelector('.slide').clientWidth;
+
+            // Cambiar la posición del slider
+            slider.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
+        }
+
+        document.querySelector('.prev').addEventListener('click', () => moveSlide(-1));
+        document.querySelector('.next').addEventListener('click', () => moveSlide(1));
+    })
+    .catch(error => console.error('Error loading projects:', error));
 });
+
+
+
+
 
